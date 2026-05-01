6732a136f6047b74f339ecbecc4faee8d9ff83a5
amine <minabd112022@gmail.com>
2026-05-02 00:19:55 +0100

    fix manuel payment + cloudinary

--- lib/upload.ts
+++ lib/upload.ts
@@ -2,8 +2,8 @@ import { v2 as cloudinary } from "cloudinary";
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD!,
-  api_key: process.env.CLOUDINARY_KEY!,
-  api_secret: process.env.CLOUDINARY_SECRET!,
+  api_key: process.env.CLOUDINARY_API_KEY!,
+  api_secret: process.env.CLOUDINARY_API_SECRET!,
 });
 
 export async function uploadImage(file: File) {
